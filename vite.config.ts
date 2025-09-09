c9f96acb404cf24d8a4f4ffa2e1c19dcefcbd855
JeongOnChoi <chjo0330@gmail.com>
2025-09-09 14:07:00 +0900

    Vercel 배포를 위한 설정 추가 및 Rollup의 Terser 미니파이 설정 적용

--- vite.config.ts
+++ vite.config.ts
@@ -5,4 +5,13 @@ import react from "@vitejs/plugin-react";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  build: {
+    target: 'esnext',
+    minify: 'terser',
+    rollupOptions: {
+      output: {
+        manualChunks: undefined,
+      },
+    },
+  },
 });
